0fb96bfb84a9eddf25ad6643c6dc5380bf85dd6f
decant09 <>
2023-12-03 20:09:46 +0000

    Edit allowed hosts

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = 'django-insecure-$xd2!v3p7cm3g+20jrae=t9f(3kcx8@sl8^u4v&ykew^jpz$!v
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'decant09-boutique-ado-1fbc20ba4984.herokuapp.com/', 'localhost']
+    'decant09-boutique-ado-1fbc20ba4984.herokuapp.com/',
+    '8000-decant09-boutique-ado-v1-5f3r5tascw.us2.codeanyapp.com']
 
 
 # Application definition
