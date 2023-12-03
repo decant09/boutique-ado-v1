36b4681a7ae16cc00195d198cb3daa2966d47545
decant09 <>
2023-12-03 19:35:59 +0000

    Edit settings for deployment

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,10 @@ SECRET_KEY = 'django-insecure-$xd2!v3p7cm3g+20jrae=t9f(3kcx8@sl8^u4v&ykew^jpz$!v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-decant09-boutique-ado-v1-5f3r5tascw.us2.codeanyapp.com']
+ALLOWED_HOSTS = [
+    '8000-decant09-boutique-ado-v1-5f3r5tascw.us2.codeanyapp.com',
+    'decant09-boutique-ado.herokuapp.com'
+    ]
 
 
 # Application definition
@@ -117,12 +121,18 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
